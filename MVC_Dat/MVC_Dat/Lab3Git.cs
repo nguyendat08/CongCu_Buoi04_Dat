98f399db68c0c14b74046d6b2bfbd955b3bf6552
“nguyendat08git config --global user.email “nguyenhuudat2308@gmail.com <nguyenhuudat2308@gmail.com>
2021-04-15 11:24:29 +0700

    Them 1 class va push len server EDITED

---
+++ MVC_Dat/MVC_Dat/Lab3Git.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Dat
+{
+    public class Lab3Git
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string GioiTinh { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address {get; set;}
+        public string NameClass { get; set; }
+        public string Country { get; set; }
+
+    }
+}
